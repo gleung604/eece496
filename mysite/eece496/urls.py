f44d7deb1e62f86bf0201634066f1daf9870b98b
Gary Leung <gleung604@gmail.com>
2013-03-24 16:23:31 -0700

    Added user login view

--- mysite/eece496/urls.py
+++ mysite/eece496/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView, UpdateView
+from django.contrib.auth.decorators import login_required
 from eece496.models import Session, Attendance, Evaluation
 
 urlpatterns = patterns('',
     url(r'^$',
-        ListView.as_view(
+        login_required(ListView.as_view(
             queryset=Session.objects.all(),
             context_object_name='session_list',
-            template_name='eece496/index.html')),
+            template_name='eece496/index.html'))),
     url(r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Session,
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^eece496/', include('eece496.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/', 'django.contrib.auth.views.login',
+        {'template_name': 'eece496/login.html'}),
 )
